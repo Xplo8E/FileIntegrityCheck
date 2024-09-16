9b53297b432734545576e89dc80e46120b6d245e
Xplo8E <rvkyadav71@gmail.com>
2024-09-16 13:34:47 +0530

    Initial Commit

---
+++ FileIntegrityCheck/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FileIntegrityCheck
+//
+//  Created by APPLE on 16/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FileIntegrityCheck/FileIntegrityCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileIntegrityCheckApp.swift
+//  FileIntegrityCheck
+//
+//  Created by APPLE on 16/09/24.
+//
+
+import SwiftUI
+
+@main
+struct FileIntegrityCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
